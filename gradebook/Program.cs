f10a1635213f2ce46315c106e134ad5c2b2bb598
Hope Ndudim <hope.ndudim@thebulb.africa>
2021-06-21 16:11:52 +0100

    Initial Gradebook Commit

---
+++ gradebook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var grades= new List<double>() {12.1, 4.6, 10.3, 3.7};
+            grades.Add(40.2);
+
+            var result=0.0;
+
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+
+            result /=grades.Count;
+
+            Console.WriteLine($"The Average Grade is {result:N1}");
+        }
+    }
+}
